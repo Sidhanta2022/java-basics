faffa5a314b4bc679e4e674ca06e710759692b58
Sidhanta2022 <97392332+Sidhanta2022@users.noreply.github.com>
2022-10-01 17:23:39 +0530

    Create java introduction.java

---
+++ java introduction.java
@@ -0,0 +1,23 @@
+public class sidhanta {
+    public static void main(String[] args) {
+        System.out.println("sidhanta");
+        String name = "suman";
+        int a = 20;
+        float b = 30.33f;
+        boolean c = true;
+        System.out.println(name);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        // rules to use variable in java
+       /*1) java is a case sensitive language
+         2) Should not contain space 
+         3) reserve keywords are not allowed*/ 
+        
+        //  data types in java
+        /*primitive = byte(1 byte), short(2 byte), int(4 byte), long(8 byte), float(4 byte), double(8 byte), boolean(1 bit), char(2 byte)
+        1 byte = 8 bit
+          non primitive (reference type) 
+         */
+    }
+}
